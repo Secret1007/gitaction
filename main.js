05b153f429ce773e85b5fe016a5d60f0f73b9b90
syq <1822845895@qq.com>
2023-01-12 17:57:48 +0800

    太难了

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
-  111122333
+  1111
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
