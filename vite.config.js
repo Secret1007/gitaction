cf6e1b08e125714e7758255387867a666888dd70
syq <1822845895@qq.com>
2023-01-16 13:07:00 +0800

    hhh

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,4 @@
-import { defineConfig, AliasOptions } from "vite";
-import eslintPlugin from "vite-plugin-eslint";
-import Components from "unplugin-vue-components/vite";
-import { VantResolver } from "unplugin-vue-components/resolvers";
-import path from "path";
-import { visualizer } from "rollup-plugin-visualizer";
-import vue from "@vitejs/plugin-vue";
-import VueMacros from "unplugin-vue-macros/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
